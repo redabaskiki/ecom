c3d491300bda7406751e02a279e0ddf521ef55b5
reda baskiki <baskikireda.rb@gmail.com>
2024-10-16 20:47:33 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
- import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1 className='text-[60px] flex-center'> hello hello my name is zrir9</h1>
-        
+        <h1 className="text-[60px] flex-center">
+          {" "}
+          hello hello my name is amine
+        </h1>
       </header>
     </div>
   );
